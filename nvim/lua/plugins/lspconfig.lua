9ba0c602e28a4f5c7e79237fc63e5717f52ea52e
bhwk <bryanhawaikit@gmail.com>
2025-02-01 02:09:51 +0800

    add nixd to plugin configuration

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -6,6 +6,20 @@ return {
         lua_ls = {
           mason = false,
         },
+        nixd = {
+          mason = false,
+          nixpkgs = {
+            expr = "import <nixpkgs> { }",
+          },
+          formatting = {
+            command = { "nixfmt" },
+          },
+          options = {
+            nixos = {
+              expr = '(builtins.getFlake("git+file://"+ toString ./.)).nixosConfigurations.${config.networking.hostName}.options',
+            },
+          },
+        },
       },
     },
   },
