2898fed8a701ccdd3e22238d364faa7d2d692ec7
bhwk <bryanhawaikit@gmail.com>
2025-12-18 18:49:31 +0800

    update dotfiles

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -1,12 +1,12 @@
 -- ~/.dotfiles/nvim/lua/plugins/mini.lua
 return {
-  {
-    "echasnovski/mini.nvim",
-    version = false, -- Use git commit for latest features
-    opts = {},
-    config = function()
-      require("mini.base16")
-    end,
-  },
+  -- {
+  --   "nvim-mini/mini.nvim",
+  --   version = false, -- Use git commit for latest features
+  --   opts = {},
+  --   config = function()
+  --     require("mini.base16")
+  --   end,
+  -- },
   -- ... other plugins ...
 }
