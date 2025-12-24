abf56c295c856ca56162095e5b9d733a8413428d
bhwk <bryanhawaikit@gmail.com>
2025-12-25 02:00:07 +0800

    disable completion for nixd

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -6,23 +6,36 @@ return {
         lua_ls = {
           mason = false,
         },
-        -- nixd = {
-        --   mason = false,
-        --   nixpkgs = {
-        --     expr = "import <nixpkgs> { }",
-        --   },
-        --   formatting = {
-        --     command = { "nixfmt" },
-        --   },
-        --   options = {
-        --     nixos = {
-        --       expr = '(builtins.getFlake("git+file://"+ toString ./.)).nixosConfigurations.${config.networking.hostName}.options',
-        --     },
-        --     home_manager = {
-        --       expr = "(import <home-manager/modules> { configuration = ~/nixos-config/home/common/default.nix; pkgs = import <nixpkgs> {}; }).options",
-        --     },
-        --   },
-        -- },
+        nixd = {
+          mason = false,
+          -- This section disables the completion capability
+          capabilities = {
+            textDocument = {
+              completion = {
+                completionItem = {
+                  snippetSupport = false,
+                },
+              },
+            },
+          },
+          on_init = function(client)
+            client.server_capabilities.completionProvider = false
+          end,
+          nixpkgs = {
+            expr = "import <nixpkgs> { }",
+          },
+          formatting = {
+            command = { "nixfmt" },
+          },
+          options = {
+            nixos = {
+              expr = '(builtins.getFlake("git+file://"+ toString ./.)).nixosConfigurations.${config.networking.hostName}.options',
+            },
+            home_manager = {
+              expr = "(import <home-manager/modules> { configuration = ~/nixos-config/home/common/default.nix; pkgs = import <nixpkgs> {}; }).options",
+            },
+          },
+        },
         nil_ls = {
           mason = false,
         },
